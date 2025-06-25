ab46be34543729cc552da49b2912858b4d19c573
hyun471 <khs101ttl@gmail.com>
2025-06-25 19:53:56 +0900

    상품 메뉴 추가

---
+++ bin/console_shoppingmall.dart
@@ -0,0 +1,5 @@
+import 'package:console_shoppingmall/console_shoppingmall.dart' as console_shoppingmall;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_shoppingmall.calculate()}!');
+}
---
+++ lib/console_shoppingmall.dart
@@ -0,0 +1,41 @@
+// List<String> products = ["셔츠", "원피스", "반팔티", "반바지", "양말"];
+// List<int> prices = [45000, 30000, 35000, 38000, 5000];
+
+// class showProducts {}
+void main() {
+  ConsoleMeau meau = ConsoleMeau();
+  meau.startMeau();
+}
+
+class ConsoleMeau {
+  String consoleMeau =
+      "[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료 ";
+  void startMeau() {
+    print(consoleMeau);
+  }
+}
+
+class Shoppingmall {
+  List<Product> products = [
+    Product("셔츠", 45000),
+    Product("원피스", 30000),
+    Product("반팔티", 35000),
+    Product("반바지", 38000),
+    Product("양말", 5000),
+  ];
+  void showProducts() {
+    for (var product in products) {
+      product.show();
+    }
+  }
+}
+
+class Product {
+  String name;
+  int price;
+  Product(this.name, this.price);
+
+  void show() {
+    print("$name / $price원");
+  }
+}
