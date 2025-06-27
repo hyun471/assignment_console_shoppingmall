bf24a4163d17b4c81bbb1d9ce9ce3ba2696dd041
hyun471 <khs101ttl@gmail.com>
2025-06-27 15:47:45 +0900

    메서드 함수 shoppingmall 클래스 안으로 넣기 및 도전과제 종료시 한번더 묻기 추가

--- bin/console_shoppingmall.dart
+++ bin/console_shoppingmall.dart
@@ -7,6 +7,7 @@ void main() {
   ConsoleMeau meau = ConsoleMeau();
   Shoppingmall item = Shoppingmall();
   bool runningLoop = true;
+  print("콘솔 쇼핑몰에 오신걸 환영합니다.");
   while (runningLoop) {
     meau.startMeau();
     String? input = stdin.readLineSync();
@@ -16,54 +17,23 @@ void main() {
         item.showProducts();
         break;
       case "2":
-        addToCart(item);
+        item.addToCart();
         break;
       case "3":
-        showTotal(item);
+        item.showTotal();
         break;
       case "4":
-        print("이용해 주셔서 갑사합니다. 안녕히 가세요~");
-        runningLoop = false;
-        break;
+        // 쇼핑몰 프로그램 종료 시 한번더 확인 기능
+        print("정말로 종료하시겠습니까? [ Y / N ]");
+        String? exitRun = stdin.readLineSync();
+        if (exit == "Y") {
+          print("이용해 주셔서 갑사합니다. 안녕히 가세요~");
+          runningLoop = false;
+          break;
+        } else if (exit == "N") {
+          print("콘솔 쇼핑몰에 오신걸 환영합니다.");
+          break;
+        }
     }
   }
 }
-
-void addToCart(Shoppingmall item) {
-  print("상품명을 입력해 주세요 !");
-  String? inputItem = stdin.readLineSync()?.trim();
-  print("입력값: $inputItem");
-  bool? itemCheck = item.products.any((product) => product.name == inputItem);
-  if (itemCheck == true) {
-    print("상품의 개수를 입력해 주세요 !");
-    String? inputItemCount = stdin.readLineSync();
-    int selectItemCount = int.parse(inputItemCount!);
-    if (selectItemCount > 0) {
-      print("장바구니에 추가되었습니다 !");
-      Product selectItem = item.products.firstWhere(
-        (product) => product.name == inputItem,
-      );
-      item.cart[selectItem] = selectItemCount;
-    } else {
-      print("상품의 개수는 1개 이상 입력해 주세요 !");
-    }
-  } else {
-    print("해당 상품은 존재하지 않습니다. 상품 목록을 확인해 주세요 !");
-  }
-} // 장바구니 담기 기능
-
-void showTotal(Shoppingmall item) {
-  int totalPrice = 0;
-  if (item.cart.isEmpty) {
-    print("장바구니에 담긴 상품이 없습니다.");
-    return;
-  } else {
-    for (var entry in item.cart.entries) {
-      Product product = entry.key;
-      int count = entry.value;
-      totalPrice += product.price * count;
-    }
-    print("장바구니에 담긴 상품의 총 가격은 $totalPrice원 입니다.");
-    return;
-  }
-} // 장바구니에 담긴 상품의 총 가격 보기 기능
--- lib/shoppingmall.dart
+++ lib/shoppingmall.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:console_shoppingmall/product.dart';
 
 class Shoppingmall {
+  // 상품 리스트화
   List<Product> products = [
     Product("Shirt", 45000),
     Product("one-piece", 30000),
@@ -9,6 +12,7 @@ class Shoppingmall {
     Product("Socks", 5000),
   ];
 
+  // 상품 장바구니로 저장 기능
   Map<Product, int> cart = {};
 
   void showProducts() {
@@ -16,4 +20,43 @@ class Shoppingmall {
       product.show();
     }
   }
-} // 상품 리스트 및 상품 장바구니로 저장 기능
+
+  void addToCart() {
+    print("상품명을 입력해 주세요 !");
+    String? inputItem = stdin.readLineSync()?.trim();
+    print("입력값: $inputItem");
+    bool? itemCheck = products.any((product) => product.name == inputItem);
+    if (itemCheck == true) {
+      print("상품의 개수를 입력해 주세요 !");
+      String? inputItemCount = stdin.readLineSync();
+      int selectItemCount = int.parse(inputItemCount!);
+      if (selectItemCount > 0) {
+        print("장바구니에 추가되었습니다 !");
+        Product selectItem = products.firstWhere(
+          (product) => product.name == inputItem,
+        );
+        cart[selectItem] = selectItemCount;
+      } else {
+        print("상품의 개수는 1개 이상 입력해 주세요 !");
+      }
+    } else {
+      print("해당 상품은 존재하지 않습니다. 상품 목록을 확인해 주세요 !");
+    }
+  } // 장바구니 담기 기능
+
+  void showTotal() {
+    int totalPrice = 0;
+    if (cart.isEmpty) {
+      print("장바구니에 담긴 상품이 없습니다.");
+      return;
+    } else {
+      for (var entry in cart.entries) {
+        Product product = entry.key;
+        int count = entry.value;
+        totalPrice += product.price * count;
+      }
+      print("장바구니에 담긴 상품의 총 가격은 $totalPrice원 입니다.");
+      return;
+    }
+  } // 장바구니에 담긴 총 가격
+}
